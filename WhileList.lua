d17b1449bae6676ffd65a0c4338f002fb56ddec9
ErtyHubCheats <155924623+ErtyHubCheats@users.noreply.github.com>
2024-06-16 18:24:08 +0000

    Update

--- WhileList.lua
+++ WhileList.lua
@@ -4,12 +4,14 @@ local lss = {
         "ArTemOp121",
         "groman64",
         "ewkere_14885",
+        "slapbatles2322",
     },
     k_list = {
         ["MarkTu_Fiw"] = "lcvKpnffUhfUpNs",
         ["ArTemOp121"] = "Dev130459",
         ["groman64"] = "sqCQHomfscHmDso",
-        ["ewkere_14885"] = "qsZkMQLt_KkouFm"
+        ["ewkere_14885"] = "qsZkMQLt_KkouFm",
+        ["slapbatles2322"] = "DGjHVGhGjXcMECP",
     }
 }
 
