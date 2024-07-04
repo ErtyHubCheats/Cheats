0a81cc77104b32c5bb1a474bc9e70b1b1e2adbb8
ErtyHubCheats <155924623+ErtyHubCheats@users.noreply.github.com>
2024-07-04 13:03:00 +0500

    Update verP.lua

--- verP.lua
+++ verP.lua
@@ -12,11 +12,23 @@ local l = {
                 ColorSequenceKeypoint.new(0,Color3.new(1,0,0,0)),
                 ColorSequenceKeypoint.new(1,Color3.new(0.694118,0,0,0)),
             },
+            ["Godly"] = ColorSequence.new{
+                ColorSequenceKeypoint.new(0,Color3.new(0 ,0, 0.588235, 0)),
+                ColorSequenceKeypoint.new(1,Color3.new(0 ,0 ,1 ,0)),
+            },
             ["Exclusive"] = ColorSequence.new{
                 ColorSequenceKeypoint.new(0,Color3.new(0.85098,0.576471 ,1 ,0)),
                 ColorSequenceKeypoint.new(1,Color3.new(0.85098 ,0.301961 ,1 ,0)),
+            },
+            ["Ultimate"] = ColorSequence.new{
+        		ColorSequenceKeypoint.new(0, Color3.new(1, 0, 0, 0)),
+        		ColorSequenceKeypoint.new(0.25, Color3.new(1, 0, 1, 0)),
+        		ColorSequenceKeypoint.new(0.5, Color3.new(0, 0, 1, 0)),
+        		ColorSequenceKeypoint.new(0.75, Color3.new(0, 1, 1, 0)),
+        		ColorSequenceKeypoint.new(1, Color3.new(0, 1, 0, 0))
             }
         },
+        
         ["units"] = {
             ["Titan Clover Man"] = {
                 "$1777",
@@ -47,6 +59,56 @@ local l = {
                 "$700",
                 "rbxassetid://17513463046",
                 "Legendary"
+            },
+            ["Ghost Cameraman"] = {
+                "$1000",
+                "rbxassetid://16049131486",
+                "Exclusive"
+            },
+            ["Titan Present Man"] = {
+                "$1000",
+                "rbxassetid://16112184181",
+                "Godly"
+            },
+            ["Upgraded Titan Cinemaman"] = {
+                "$3000",
+                "rbxassetid://16111271244",
+                "Godly"
+            },
+            ["Mech Bunny Titan"] = {
+                "$2500",
+                "rbxassetid://16765656917",
+                "Godly"
+            },
+            ["Upgraded Titan Drill Man"] = {
+                "$4000",
+                "rbxassetid://16427931085",
+                "Ultimate"
+            },
+            ["Upgraded Titan Clockman"] = {
+                "$3500",
+                "rbxassetid://17451115710",
+                "Ultimate"
+            },
+            ["DJ TV Man"] = {
+                "$300",
+                "rbxassetid://16070423351",
+                "Exclusive"
+            },
+            ["Chief Clockman"] = {
+                "$1500",
+                "rbxassetid://17448163699",
+                "Godly"
+            },
+            ["Golden Future Large Clockman"] = {
+                "$2500",
+                "rbxassetid://17780953008",
+                "Godly"
+            },
+            ["Exclusive Scientist Crate"] = {
+                "",
+                "rbxassetid://14199484613",
+                "Exclusive"
             },
         }
 }
