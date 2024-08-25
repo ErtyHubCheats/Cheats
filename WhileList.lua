47e70a7a6e134f59228c04c27fbffeac915e9c98
ErtyHubCheats <155924623+ErtyHubCheats@users.noreply.github.com>
2024-08-25 09:08:43 +0500

    Update WhileList.lua

--- WhileList.lua
+++ WhileList.lua
@@ -1,21 +1,8 @@
 local lss = {
-    ["p_list"] = {
-        "MarkTu_Fiw",
-        "Bot_1Places",
-        "ArTemOp122",
-        "groman64",
-        "slapbatles2322",
-        "rostilikom5006",
-        "daniil098905",
-    },
     ["k_list"] = {
-        ["MarkTu_Fiw"] = "lcvKpnffUhfUpNs",
         ["Bot_1Places"] = "Dev",
-        ["ArTemOp122"] = "Dev",
-        ["groman64"] = "sqCQHomfscHmDso",
-        ["slapbatles2322"] = "DGjHVGhGjXcMECP",
-        ["rostilikom5006"] = "xjCDRoGcPNEHWQm",
-        ["daniil098905"] = "_kEHVGhGoPDMECP"
+        ["Deat_her"] = "Dev",
+        ["zuikova101101"] = "Dev",
     }
 }
 
