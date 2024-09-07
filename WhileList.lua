e0a0f82153db4dfd47ea7fca3f0012b6897cd0af
ErtyHubCheats <155924623+ErtyHubCheats@users.noreply.github.com>
2024-09-08 00:45:05 +0500

    Update WhileList.lua

--- WhileList.lua
+++ WhileList.lua
@@ -4,7 +4,8 @@ local lss = {
         ["Deat_her"] = "Dev",
         ["zuikova101101"] = "Dev",
         ["slapbatles2322"] = "udf_EICtejavaJ_",
-        ["MarkTu_Fiw"] = "MHNMahWtpWgvqff",
+        ["MarkTu_Fiw"] = "COwner",
+        ["MarkTop222228"] = "COwner",
     }
 }
 
